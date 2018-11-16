7e665d537987732d8cd9f747ac9cbc16085ab4cc
kicheol <emfla122@gmail.com>
2018-11-17 03:31:52 +0900

    frag2, asdf1 DB예제코드추가

--- app/src/main/java/com/example/kicheol/myapplication/frag2.java
+++ app/src/main/java/com/example/kicheol/myapplication/frag2.java
@@ -1,5 +1,6 @@
 package com.example.kicheol.myapplication;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,15 +9,137 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class frag2 extends Fragment {
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ADD = "address";
 
+    String myJSON;
+    JSONArray peoples = null;
+    ArrayList<HashMap<String, String>> personList;
+
+    TextView ID;
+    TextView NAME;
+    TextView ADDRESS;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.asdf1, container, false);
-        EditText a = (EditText) rootView.findViewById(R.id.editText);
+
+        ID = rootView.findViewById(R.id.id);
+        NAME = rootView.findViewById(R.id.name);
+        ADDRESS = rootView.findViewById(R.id.address);
+
+
+        //list = (ListView) rootView.findViewById(R.id.listView);
+        personList = new ArrayList<HashMap<String, String>>();
+        getData("http://192.168.0.23/PHP_connection.php"); //수정 필요
+//        @Override
+//        protected void onCreate (Bundle savedInstanceState){
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_main);
+//
+//        }
+
+
+
 
         return rootView;
+    }
+    protected void showList () {
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for (int i = 0; i < peoples.length(); i++) {
+                JSONObject c = peoples.getJSONObject(i);
+                String id = c.getString(TAG_ID);
+                String name = c.getString(TAG_NAME);
+                String address = c.getString(TAG_ADD);
+
+                HashMap<String, String> persons = new HashMap<String, String>();
+
+                persons.put(TAG_ID, id);
+                persons.put(TAG_NAME, name);
+                persons.put(TAG_ADD, address);
+
+                personList.add(persons);
+
+
+            }
+            ID.setText(personList.get(0).get(TAG_ID));
+            NAME.setText(personList.get(0).get(TAG_NAME));
+            ADDRESS.setText(personList.get(0).get(TAG_ADD));
+
+//            ListAdapter adapter = new SimpleAdapter(
+//                    MainActivity.this, personList, R.layout.list_item,
+//                    new String[]{TAG_ID, TAG_NAME, TAG_ADD},
+//                    new int[]{R.id.id, R.id.name, R.id.address}
+//            );
+
+//            list.setAdapter(adapter);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getData (String url){
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+
+
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                myJSON = result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
     }
 }
